4a8919a11b8935e53c91059136a092a002f97835
Patrick Mwaniki <patrickmwanikk@gmail.com>
2023-06-07 16:18:14 +0300

    add bootsrap and icons

--- wp-content/themes/easymanage/functions.php
+++ wp-content/themes/easymanage/functions.php
@@ -2,6 +2,16 @@
 
 function easymanagestyles () {
     wp_enqueue_style('customcss',get_template_directory_uri().'/custom/custom.css',[],'1.0.0','all');
+
+    //introducing bootstrap
+    wp_register_style('bootstrapcss', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css', [], '5.2.3', 'all');
+    wp_enqueue_style('bootstrapcss');
+
+    wp_register_script('jsbootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js', [], '5.2.3', false);
+    wp_enqueue_script('jsbootstrap');
+   //bootsrap icons
+    wp_register_script('bootstrapicons','https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css',[],'1.10.5',false);
+
 }
 add_action('wp_enqueue_scripts','easymanagestyles');
 
--- wp-content/themes/easymanage/login/page-login.php
+++ wp-content/themes/easymanage/login/page-login.php
@@ -6,4 +6,12 @@
  */
 
  ?>
+ 
 <?php wp_head(); ?>
+
+<section class="container-login">
+    <div class="">
+        <h1>ggg <i class="bi bi-wordpress"></i></h1>
+    
+</div>
+</section>
